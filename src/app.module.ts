89be0d5bfd74c50acd01344a686a3a64ca559ce1
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-17 19:17:39 -0800

    Generate module reports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
