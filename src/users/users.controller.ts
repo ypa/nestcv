7153ed67dd982229a137b8826c4a656ccf77baf1
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-26 19:49:45 -0800

    Apply Serialize decorator at the controller level

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) { }
 
@@ -14,7 +15,6 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     console.log('Handler is running')
