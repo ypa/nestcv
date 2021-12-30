56dddc4ee7345e60882bc5d46ab57706cd096b47
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-29 19:19:43 -0800

    Finish signup process with a step to hash user's password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -13,10 +17,23 @@ export class AuthService {
     }
 
     // Hash the users password
+    // Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    // Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed result and the salt together
+    const result = salt + '.' + hash.toString('hex');
 
     // Create a new user and save it
+    const user = await this.usersService.create(email, result);
 
     // return the user
+    return user;
   }
 
+  signin() {
+
+  }
 }
