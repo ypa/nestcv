17c299818129266c3a64c149cdccc9bc85a8fdd2
Yan P Aung <yan.pye.aung@gmail.com>
2021-12-30 19:54:19 -0800

    Import and configure cookie session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// using old style require because of mismatch between nest and cookie-session libs.
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['asdfasdfasdf']
+  }));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
