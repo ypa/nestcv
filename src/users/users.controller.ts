600b382fb50d742231ccdcad719625295be8df00
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-01 20:10:48 -0800

    Add AuthGuard and use that to guard the whoami user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, Session } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, Session, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -7,21 +7,15 @@ import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
+import { AuthGuard } from '../gurads/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService, private authService: AuthService) { }
 
-  // @Get('/whoami')
-  // whoAmI(@Session() session: any) {
-  //   const user = this.usersService.findOne(session.userId);
-  //   if (!user) {
-  //     throw new NotFoundException("User not found")
-  //   }
-  //   return user;
-  // }
   @Get('whoami')
+  @UseGuards(AuthGuard)
   whoAmI(@CurrentUser() user: User) {
     return user;
   }
