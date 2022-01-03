3b0fc5b3d4a413e3fc709fd172ad9d38178ede28
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-02 20:34:34 -0800

    Add auth service spec unit test

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+it('can create an instance of auth service', async () => {
+  // create a fake copy of the users service
+  const fakeUsersService = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password })
+  }
+
+  // create DI container
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService
+      }
+    ],
+  }).compile();
+
+  // create auth service
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
+})
