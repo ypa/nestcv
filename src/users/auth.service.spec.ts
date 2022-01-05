ed4343b23dea5b7fcd2a7d023e31c539ba2038cf
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-04 20:34:22 -0800

    Add type signature to make fakerUserService stricter type checking

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
 it('can create an instance of auth service', async () => {
   // create a fake copy of the users service
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password })
+      Promise.resolve({ id: 1, email, password } as User)
   }
 
   // create DI container
