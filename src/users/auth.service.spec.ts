a6a414384082a05d686b5da4badf5f5cd320022b
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-05 20:27:49 -0800

    Give proper structure to test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,27 +3,34 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-it('can create an instance of auth service', async () => {
-  // create a fake copy of the users service
-  const fakeUsersService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User)
-  }
+describe('AuthService', () => {
+  let service: AuthService;
 
-  // create DI container
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService
-      }
-    ],
-  }).compile();
+  beforeEach(async () => {
+    // create a fake copy of the users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User)
+    }
 
-  // create auth service
-  const service = module.get(AuthService);
+    // create DI container
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        }
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
-})
+    // create auth service
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  })
+
+});
