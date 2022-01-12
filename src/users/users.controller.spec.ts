bdca53a0204e95851b0a0614dcee0a0f91cf3df7
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-11 19:53:07 -0800

    Add signin session test

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -23,7 +23,9 @@ describe('UsersController', () => {
 
     fakeAuthService = {
       // signup: () => { },
-      // signin: () => { }
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User)
+      }
     }
 
     const module: TestingModule = await Test.createTestingModule({
@@ -67,4 +69,13 @@ describe('UsersController', () => {
       done();
     }
   });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin({ email: 'asdf@asdf.com', password: 'asdf' },
+      session);
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  })
 });
