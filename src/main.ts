3fafd501e14df339480a9211b2442286862872ad
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-12 20:15:03 -0800

    Refactor for e2e: move cookieSession into app module

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-// using old style require because of mismatch between nest and cookie-session libs.
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['asdfasdfasdf']
-  }));
 
   await app.listen(3000);
 }
