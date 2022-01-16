a7b9366ee49a428d54d4d50f516e14e3278cad97
Yan P Aung <yan.pye.aung@gmail.com>
2022-01-15 19:58:21 -0800

    Add cookie test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,22 @@ describe('Authentication System (e2e)', () => {
         expect(email).toEqual(email);
       })
   });
+
+  it('sign up as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email);
+  })
 });
